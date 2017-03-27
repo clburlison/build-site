5c7fbc14da232c93bdbb8ea562597372df851d5a
Clayton Burlison <github@clburlison.com>
2017-03-27 18:52:47 -0500

    Adjust build scripts
    * go get -u on micromdm so we pull updates
    * comments on scripts
    * allow make_release.sh to work on macOS

--- make_release.sh
+++ make_release.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+# This script is called via a Github webhook when changes are made to the
+# master branch. It builds the binaries (via micromdm_build.sh) then wraps the
+# output directory to a zip file. Then moves to the web share.
 set -x
 source_path=$(dirname "${0}"); cd "${source_path}"
-docker pull golang:1.8
 
-docker run --rm -v "$(pwd)"/go:/go golang:1.8 \
-  /go/micromdm_build.sh
+docker pull golang:1.8
+docker run --rm -v "$(pwd)"/go:/go \
+  -v "$(pwd)"/micromdm_build.sh:/micromdm_build.sh \
+  golang:1.8 \
+  /micromdm_build.sh
 
 cd go/src/github.com/micromdm/micromdm
-VERSION="$(build/micromdm-linux-amd64 version | awk '{print $3}')"
-# VERSION="$(build/micromdm-darwin-amd64 version | awk '{print $3}')"
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+  VERSION="$(build/micromdm-linux-amd64 version | awk '{print $3}')"
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  VERSION="$(build/micromdm-darwin-amd64 version | awk '{print $3}')"
+else
+  exit 1
+fi
 zip -r "$VERSION".zip build
 
+# This sed command does not provide a Darwin safe json file
 cat << EOF > "$VERSION".json
 {
   "git_commit": "$(git rev-parse HEAD)",
--- micromdm_build.sh
+++ micromdm_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# This script downloads micromdm + dependencies to build the binaries
 set -x
 go get -u github.com/golang/dep/...
-go get github.com/micromdm/micromdm
+go get -u github.com/micromdm/micromdm
 cd $GOPATH/src/github.com/micromdm/micromdm
 dep ensure
 ./release.sh
